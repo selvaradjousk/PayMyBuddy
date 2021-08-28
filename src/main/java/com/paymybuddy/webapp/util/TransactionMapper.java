f630f120298aeb5e17095313da211bc9d2282a45
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-08-28 22:17:43 +0200

    [TEST + FEAT] Unit Tests Transaction Mapper -> DONE

--- src/main/java/com/paymybuddy/webapp/util/TransactionMapper.java
+++ src/main/java/com/paymybuddy/webapp/util/TransactionMapper.java
@@ -1,18 +1,44 @@
 package com.paymybuddy.webapp.util;
 
+import java.time.LocalDate;
+
+import org.springframework.stereotype.Component;
+
+import com.paymybuddy.webapp.dto.TransactionDTO;
+import com.paymybuddy.webapp.model.Transaction;
+
+@Component
 public class TransactionMapper {
 
-
-	// **************************** TODOs LIST ***********************************
+	LocalDate createDate = LocalDate.now();	
 	
-	// Method: c
-	// -->
-	// --> 
-	// --> 
-	// --> 
-	// --> 
-	// --> 
-	// --> 
-	// --> 
-	// --> 
+	  public Transaction toTransactionDO(TransactionDTO transactionDTO) {
+
+	        Transaction transaction = new Transaction();
+
+	        transaction.setIdTransaction(transactionDTO.getIdTransaction());
+	        transaction.setPayer(transactionDTO.getPayer());
+	        transaction.setBeneficiary(transactionDTO.getBeneficiary());
+	        transaction.setAmount(transactionDTO.getAmount());
+	        transaction.setDescription(transactionDTO.getDescription());
+	        transaction.setCreationDate(transactionDTO.getCreationDate());
+	        transaction.setCommision(transactionDTO.getCommision());
+
+	        return transaction;
+	    }
+
+	    public TransactionDTO toTransactionDTO(Transaction transaction) {
+
+	        TransactionDTO transactionDTO = new TransactionDTO();
+
+	        transactionDTO.setIdTransaction(transaction.getIdTransaction());
+	        transactionDTO.setPayer(transaction.getPayer());
+	        transactionDTO.setBeneficiary(transaction.getBeneficiary());
+	        transactionDTO.setAmount(transaction.getAmount());
+	        transactionDTO.setDescription(transaction.getDescription());
+	        transactionDTO.setCreationDate(transaction.getCreationDate());
+	        transactionDTO.setCommision(transaction.getCommision());
+
+	        return transactionDTO;
+	    }
 }
---
+++ src/test/java/com/paymybuddy/webapp/unitTest/util/TransactionMapperTest.java
@@ -0,0 +1,68 @@
+package com.paymybuddy.webapp.unitTest.util;
+
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import com.paymybuddy.webapp.dto.TransactionDTO;
+import com.paymybuddy.webapp.model.Transaction;
+import com.paymybuddy.webapp.model.User;
+import com.paymybuddy.webapp.util.TransactionMapper;
+
+	class TransactionMapperTest {
+
+
+		User user = new User(
+	    		100,
+	            "testUserName",
+	            "testFirstName",
+	            "testLastName",
+	            "myEmail",
+	            "myPassword",
+	            LocalDate.parse("2019-12-31"),
+	            LocalDate.parse("2019-12-31"),
+	            "admin",
+	            true,
+	            1000.0);
+
+		@Test
+	    public void testToEntity() {
+		TransactionDTO dto = new TransactionDTO(
+				user,
+				user,
+				1000.0,
+				"testDescription",
+				LocalDate.parse("2019-12-31"),
+				0.25);
+		
+		TransactionMapper mapper = new TransactionMapper();
+		Transaction entity = mapper.toTransactionDO(dto);
+		
+		assertEquals(entity.getPayer(), dto.getPayer());
+		}
+		
+		@Test
+	    public void testToDTO() {
+			
+		Transaction entity = new Transaction(
+				10,
+				user,
+				user,
+				1000.0,
+				"testDescription",
+				LocalDate.parse("2019-12-31"),
+				0.25);
+		
+		TransactionMapper mapper = new TransactionMapper();
+		TransactionDTO dto = mapper.toTransactionDTO(entity);
+		
+		assertEquals(dto.getPayer(), entity.getPayer());
+		assertEquals(dto.getDescription(), entity.getDescription());
+		assertEquals(dto.getAmount(), entity.getAmount());
+		}
+
+}
+
