5878405cac37f613b510fbbef08646438a053a06
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-09-05 23:37:58 +0200

    [TEST CASE] Controller - HOME: GET Home Url With Authentication - (200) Http Response code ==> PASSED

--- src/test/java/com/paymybuddy/webapp/unitTest/controller/HomeControllerWebMvcTest.java
+++ src/test/java/com/paymybuddy/webapp/unitTest/controller/HomeControllerWebMvcTest.java
@@ -2,12 +2,14 @@ package com.paymybuddy.webapp.unitTest.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -65,7 +67,18 @@ class HomeControllerWebMvcTest {
 			.andExpect(redirectedUrl("http://localhost/login"));
 		}
 
-		
+		// ********************************************************************
+
+		@WithMockUser("testemail1@email.com")
+		@Test
+		public void testHomeUrlWithLoginStatusOK() throws Exception {
+
+			mockMvc.perform(get("/home"))
+			 .andExpect(status().isOk());
+			
+		}
+	    
+		// ********************************************************************		
 		
     
     
