cd62de7513a06155df4ad34d80b5ad3794352a17
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-09-05 23:36:34 +0200

    [TEST CASE] Controller - HOME: GET Home Url Without Authentication - redirect (302) Http Response code ==> PASSED

--- src/test/java/com/paymybuddy/webapp/unitTest/controller/HomeControllerWebMvcTest.java
+++ src/test/java/com/paymybuddy/webapp/unitTest/controller/HomeControllerWebMvcTest.java
@@ -1,48 +1,73 @@
 package com.paymybuddy.webapp.unitTest.controller;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 // WebMvcTest(controllers = HelloController.class) - load only the controller by listing here
-@RunWith(SpringRunner.class)
-@WebMvcTest
+@DisplayName("Home CONTROLLER - H2 DB TEST ")
+@AutoConfigureMockMvc
+@SpringBootTest
 @ActiveProfiles("test")
 class HomeControllerWebMvcTest {
 
     @Autowired
     private MockMvc mockMvc;
-
-    @Test
-    public void testHomeShouldReturnMessage() throws Exception {
-    mockMvc.perform(MockMvcRequestBuilders.get("/home"))
-    .andExpect(MockMvcResultMatchers.status().is(200))
-    .andExpect(MockMvcResultMatchers.content().string("home"))
-    .andExpect(MockMvcResultMatchers.header().string("Content-Type", "text/plain;charset=UTF-8"))
-    .andExpect(MockMvcResultMatchers.header().string("Content-Length", "4")); 
-//    .andExpect(model().attribute("msg", equalTo("SUCCESS LODAING HOME PAGE")))
-    }
-
-    @Test
-    public void testLoginShouldReturnMessage() throws Exception {
-    mockMvc.perform(MockMvcRequestBuilders.get("/login"))
-    .andExpect(MockMvcResultMatchers.status().is(200))
-    .andExpect(MockMvcResultMatchers.content().string("login"))
-    .andExpect(MockMvcResultMatchers.header().string("Content-Type", "text/plain;charset=UTF-8"))
-    .andExpect(MockMvcResultMatchers.header().string("Content-Length", "5")); 
-    }
     
-    @Test
-    public void testLogoutShouldReturnMessage() throws Exception {
-    mockMvc.perform(MockMvcRequestBuilders.get("/logout"))
-    .andExpect(MockMvcResultMatchers.status().is(302)); 
-    }
+	@Autowired
+	ObjectMapper mapper;
+    
+    
+
+//    @Test
+//    public void testHomeShouldReturnMessage() throws Exception {
+//    mockMvc.perform(MockMvcRequestBuilders.get("/home"))
+//    .andExpect(MockMvcResultMatchers.status().is(200))
+//    .andExpect(MockMvcResultMatchers.content().string("home"))
+//    .andExpect(MockMvcResultMatchers.header().string("Content-Type", "text/plain;charset=UTF-8"))
+//    .andExpect(MockMvcResultMatchers.header().string("Content-Length", "4")); 
+////    .andExpect(model().attribute("msg", equalTo("SUCCESS LODAING HOME PAGE")))
+//    }
+//
+//    @Test
+//    public void testLoginShouldReturnMessage() throws Exception {
+//    mockMvc.perform(MockMvcRequestBuilders.get("/login"))
+//    .andExpect(MockMvcResultMatchers.status().is(200))
+//    .andExpect(MockMvcResultMatchers.content().string("login"))
+//    .andExpect(MockMvcResultMatchers.header().string("Content-Type", "text/plain;charset=UTF-8"))
+//    .andExpect(MockMvcResultMatchers.header().string("Content-Length", "5")); 
+//    }
+//    
+//    @Test
+//    public void testLogoutShouldReturnMessage() throws Exception {
+//    mockMvc.perform(MockMvcRequestBuilders.get("/logout"))
+//    .andExpect(MockMvcResultMatchers.status().is(302)); 
+//    }
+//    
+    
+    
+	  
+		// ********************************************************************
+
+		@Test
+		public void testHomeUrlWithoutAuthentication() throws Exception {
+
+			mockMvc.perform(get("/home"))
+			.andExpect(redirectedUrl("http://localhost/login"));
+		}
+
+		
+		
+    
     
     
 }
