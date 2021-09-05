17705f2dc16a98cc3602280589f43652b863aad5
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-09-06 01:14:06 +0200

    [TEST CASE] Controller - USER : POST /register empty confirmation password input URL request (302) redirect /register PASSWORD MISMATCH error Http Response code ==> PASSED

--- src/test/java/com/paymybuddy/webapp/unitTest/controller/UserControllerTest.java
+++ src/test/java/com/paymybuddy/webapp/unitTest/controller/UserControllerTest.java
@@ -163,6 +163,27 @@ class UserControllerTest {
  }
 	
 	
+	// ********************************************************************
 	
+
+		@DisplayName("REGISTER POST request empty Confirmation Password input Response - 302 - Password Mismatch "
+				+ "GIVEN POST request url /register empty Confirmation Password"
+				+ "WHEN Requested POST /register "
+				+ "THEN returns expected 302 redirect /register http response -  Password Mismatch") 
+		@Test
+		public void testAddNewUserPostRequestEmptyConfirmationPassword() throws Exception {
+		     mockMvc.perform(post("/register")
+		             .param("firstName","testFirstname")
+		             .param("lastName", "testLastname")
+		             .param("email", "testemail@email.com")
+		             .param("password", "testpassword")
+		             .param("confirmation", ""))
+		     		.andExpect(status().isFound())
+		     		.andExpect(model().hasNoErrors())
+		     		.andExpect(view().name("redirect:/register?&firstName=testFirstname&lastName=testLastname&email=testemail@email.com&password=testpassword&confirmation=&errorMessage=Password MISMATCH"));
+	 }
+		
+		
+		
 	
 }
