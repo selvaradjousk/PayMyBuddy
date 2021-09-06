7a6ba89b9dbc811eed7cc5b38946d7349a9262af
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-09-06 17:57:50 +0200

    [TEST] Controller - TRANSACTION : GET /transaction page request with AUTH (200 OK) http Response expected ==> FAILED

--- src/test/java/com/paymybuddy/webapp/unitTest/controller/ContactTransactionControllerTest.java
+++ src/test/java/com/paymybuddy/webapp/unitTest/controller/ContactTransactionControllerTest.java
@@ -1,14 +1,17 @@
 package com.paymybuddy.webapp.unitTest.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -37,4 +40,24 @@ class ContactTransactionControllerTest {
 	}
 
 
+	// ********************************************************************
+
+	@DisplayName("/transaction page Url request with authentication 200 OK - "
+			+ "GIVEN home url /transaction "
+			+ "WHEN Requested GET /transaction page"
+			+ "THEN returns expected reponse 200 OK")  
+	@WithMockUser("testemail1@email.com")
+	@Test
+	public void testTransactionUrlWithLoginStatusOK() throws Exception {
+//		List<Transaction> transactionsList = new ArrayList<>();
+		mockMvc.perform(get("/transaction"))
+		 .andExpect(status().isOk())
+		 .andExpect(view().name("transaction"))
+		 .andExpect(model().hasNoErrors());
+		
+	}
+    
+	// ********************************************************************
+	
+	
 }
