ab6197ce30877c086ad463fd0aa1a6cb41ef6d89
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-09-06 02:02:30 +0200

    [FEAT] Controller - USER Profile FUNCTIONALITY ==> IMPLEMENTED

--- src/main/java/com/paymybuddy/webapp/controller/ProfileController.java
+++ src/main/java/com/paymybuddy/webapp/controller/ProfileController.java
@@ -1,27 +1,164 @@
 package com.paymybuddy.webapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.paymybuddy.webapp.dto.UserDTO;
+import com.paymybuddy.webapp.service.IUserService;
+
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * The Class ProfileController.
+ */
+@Log4j2
+@Controller
 public class ProfileController {
 
 
-	// **************************** TODOs LIST ***********************************
-	
-	// Method: @GetMapping("/profile}")   contains these elements in the page:
-	// --> Session authetication check for validity of session
-	// --> User DO info (GET Mapping of user info) -> DTO serves the view by getting info from DO
-	// --> on save PUT Mapping of user updated info (profile @alias User) 
-	// --> security credentials updated (PUT request)
-	// --> 
-	// --> 
-	// --> 
-	// --> 
-	// --> 
-	// --> 
+    /** The user service. */
+	@Autowired
+    IUserService userService;
 
-	// --> REFERENCES & GUIDES
-	// --> https://stackoverflow.com/questions/15493237/how-to-correctly-update-the-login-date-time-after-successful-login-with-spring-s
-	// --> https://www.baeldung.com/registration-with-spring-mvc-and-spring-security
-	// --> https://www.codebyamir.com/blog/user-account-registration-with-spring-boot
-	// --> https://stackoverflow.com/questions/50088395/what-is-the-best-way-to-update-user-data-spring-boot
-	// --> https://www.kindsonthegenius.com/spring-boot/how-to-user-registration-page-in-spring-boot-java/
-	
+    /**
+     * Instantiates a new profile controller.
+     *
+     * @param userService the user service
+     */
+    public ProfileController(IUserService userService) {
+		this.userService = userService;
+	}
+
+
+
+	// ************************************************************************
+
+    /**
+	 * Login.
+	 *
+	 * @param model the model
+	 * @param errorMessage the error message
+	 * @param firstName the first name
+	 * @param lastName the last name
+	 * @param email the email
+	 * @param password the password
+	 * @param confirmation the confirmation
+	 * @return the string
+	 */
+	@GetMapping({ "/profile" })
+    public String login(
+    		Model model,
+    		String errorMessage,
+    		String firstName,
+    		String lastName,
+    		String email,
+    		String password,
+    		String confirmation)
+    {
+    	log.info(" Request - Get /profile ");
+
+    	String emailSession = SecurityContextHolder
+    			.getContext()
+    			.getAuthentication()
+    			.getName();
+ 
+    	log.info(" Session email by SecurityContextHolder: " + emailSession);
+    	
+    	UserDTO userLogDTO = userService
+    			.findUserByEmail(emailSession);
+
+    	log.info(" userService.findUserByEmail: "
+    	+ userLogDTO.getUserName());
+
+    	firstName = userLogDTO.getFirstName();
+        lastName = userLogDTO.getUserName();
+        email = userLogDTO.getEmail();
+        password = userLogDTO.getPassword();
+        confirmation = password;
+
+    	log.info(" fetch userInfo from userLogDTO "
+    	+ " firstname" + userLogDTO.getFirstName()
+    	+ " username" + userLogDTO.getUserName()
+    	+ " email" + userLogDTO.getEmail()
+    	+ " password" + userLogDTO.getPassword()
+    	+ " confirmation password" + userLogDTO.getPassword());
+
+        String role = isUserRoleAdminCheck(userLogDTO);
+
+        log.info(" ====> GATHER DATA MODEL FOR PROFILE PAGE LOADING<==== ");
+        
+        addDataToUserModel(
+        		model,
+        		errorMessage,
+        		firstName, lastName,
+        		email,
+        		password,
+        		confirmation,
+        		role);
+
+        log.info(" ====> GET REQUEST TO LAUNCH PROFILE PAGE"
+        		+ " - Get /profile SUCCESS<==== ");
+
+        return "profile";
+    }
+    // ************************************************************************
+
+
+
+	/**
+     * Adds the data to user model.
+     *
+     * @param model the model
+     * @param errorMessage the error message
+     * @param firstName the first name
+     * @param lastName the last name
+     * @param email the email
+     * @param password the password
+     * @param confirmation the confirmation
+     * @param role the role
+     */
+    private String addDataToUserModel(
+    		Model model,
+    		String errorMessage,
+    		String firstName,
+    		String lastName,
+    		String email,
+			String password,
+			String confirmation,
+			String role) {
+
+    	model.addAttribute("firstName", firstName);
+        model.addAttribute("lastName", lastName);
+        model.addAttribute("email", email);
+        model.addAttribute("password", password);
+        model.addAttribute("confirmation", confirmation);
+        model.addAttribute("admin", role);
+        model.addAttribute("errorMessage", errorMessage);
+
+        return "profile";
+	}
+
+    // ************************************************************
+
+
+	/**
+     * Checks if is user role admin check.
+     *
+     * @param userLogDTO the user log DTO
+     * @return the string
+     */
+    private String isUserRoleAdminCheck(UserDTO userLogDTO) {
+		String role = null;
+        String authorisation = userLogDTO
+        		.getRoles();
+        if ( authorisation.equals("ROLE_ADMIN") ) {
+            role = "admin";
+        }
+		return role;
+	}
+
+	// ************************************************************************
 }
---
+++ src/test/java/com/paymybuddy/webapp/unitTest/controller/ProfileControllerTest.java
@@ -0,0 +1,14 @@
+package com.paymybuddy.webapp.unitTest.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProfileControllerTest {
+
+	@Test
+	final void test() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
