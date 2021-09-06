71d449dd929731499ab05b5a8c701b6f747bb4a7
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-09-06 12:59:37 +0200

    [TEST] Controller - BANK TRANSFER : GET /transfer request with AUTH - 200 OK expected Http Response ==> FAILED

--- src/test/java/com/paymybuddy/webapp/unitTest/controller/BankTransferControllerTest.java
+++ src/test/java/com/paymybuddy/webapp/unitTest/controller/BankTransferControllerTest.java
@@ -1,15 +1,18 @@
 package com.paymybuddy.webapp.unitTest.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -27,6 +30,10 @@ class BankTransferControllerTest {
     
 	// ********************************************************************
 
+	@DisplayName("GET /transfer page Url request without authentication redirect /login - "
+			+ "GIVEN home url /transfer "
+			+ "WHEN Requested GET /transfer page"
+			+ "THEN returns expected reponse redirect to / login")   
 	@Test
 	public void testBankTransferUrlWithoutLoginRedirectToLoginUrl() throws Exception {
 
@@ -36,4 +43,25 @@ class BankTransferControllerTest {
 		.andExpect(redirectedUrl("http://localhost/login"));
 	}
 
+	// ********************************************************************
+
+	@DisplayName("GET /transfer page Url request with authentication (200) OK /transfer - "
+			+ "GIVEN home url /transfer "
+			+ "WHEN Requested GET /transfer page"
+			+ "THEN returns expected reponse 200 OK")   
+	@WithMockUser("testemail1@email.com")
+	@Test
+	public void testBankTransferUrlWithLoginStatusOK() throws Exception {
+
+		mockMvc.perform(get("/transfer"))
+		.andExpect(model().hasNoErrors())
+		.andExpect(view().name("transfer"))
+		 .andExpect(status().isOk());
+		
+	}
+	
+	
+	
+	
+	
 }
