e577bf3ca22c8e614f95c60783b36f5c71a19bc8
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-09-06 10:56:39 +0200

    [TEST CASE] Controller - BANK ACCOUNT : GET /bankaccounts request without auth - (302) redirect /login Http Response ==> PASSED

--- src/main/java/com/paymybuddy/webapp/controller/BankAccountController.java
+++ src/main/java/com/paymybuddy/webapp/controller/BankAccountController.java
@@ -1,19 +1,65 @@
 package com.paymybuddy.webapp.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.paymybuddy.webapp.repository.BankAccountRepository;
+import com.paymybuddy.webapp.service.IBankAccountService;
+import com.paymybuddy.webapp.util.BankAccountMapper;
+import com.paymybuddy.webapp.util.UserMapper;
+
+import lombok.extern.log4j.Log4j2;
+
+/**
+ * The Class BankAccountController - Creates REST endpoints
+ *  for operations on bank account data.
+ *  @author Senthil
+ */
+@Log4j2
+@RestController
+@RequestMapping("manage")
 public class BankAccountController {
 
-	// **************************** TODOs LIST ***********************************
 	
-	// Method: 
-	// -->  @GetMapping("/bankAccounts")  Lists the bank accountss
-	// -->  @POSTMapping("/AddBankAccount")  Add a bank account 
-	// -->  @DeleteMapping("deleteBankAccount) DELETEs a bank account
+
+    /** The bank account repository. */
+	@Autowired
+    BankAccountRepository bankAccountRepository;
+
+	/** The bank account service. */
+	@Autowired
+    IBankAccountService bankAccountService;
+
+    /** The user mapper. */
+    public UserMapper userMapper= new UserMapper();
+
+    /** The bank account mapper. */
+    public BankAccountMapper bankAccountMapper = new BankAccountMapper();
+
+    /**
+     * Constructor of class BankAccountController.
+     * Instantiates a new bank account controller.
+     *
+     * @param bankAccountRepository the bank account repository
+     * @param bankAccountService the bank account service
+     * @param userMapper the user mapper
+     * @param bankAccountMapper the bank account mapper
+     */
+    public BankAccountController(
+    		BankAccountRepository bankAccountRepository,
+    		IBankAccountService bankAccountService,
+			UserMapper userMapper,
+			BankAccountMapper bankAccountMapper) {
+		this.bankAccountRepository = bankAccountRepository;
+		this.bankAccountService = bankAccountService;
+		this.userMapper = userMapper;
+		this.bankAccountMapper = bankAccountMapper;
+	}
 
 
-	// --> REFERENCES & GUIDES
-	// --> https://docs.spring.io/spring-framework/docs/4.2.x/spring-framework-reference/html/transaction.html
-	// --> https://www.baeldung.com/transaction-configuration-with-jpa-and-spring
+    // ************************************************************************
 
-	
-	
+
+
 }
---
+++ src/test/java/com/paymybuddy/webapp/unitTest/controller/BankAccountControllerTest.java
@@ -0,0 +1,61 @@
+package com.paymybuddy.webapp.unitTest.controller;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrlPattern;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.paymybuddy.webapp.controller.BankAccountController;
+import com.paymybuddy.webapp.repository.UserRepository;
+import com.paymybuddy.webapp.service.IBankAccountService;
+
+@DisplayName("BANK ACCOUNT CONTROLLER - H2 DB TEST ")
+@AutoConfigureMockMvc
+@SpringBootTest
+@ActiveProfiles("test")
+class BankAccountControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Autowired
+    UserRepository userRepository;
+    
+    @Autowired
+    IBankAccountService bankAccountService;
+    
+    @Autowired
+    BankAccountController bankAccountController;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+ 
+  
+    
+    //********************************************************************
+    
+    @DisplayName("Bank Account Url load request Without Authentication - "
+			+ "GIVEN home url /bankAccounts "
+			+ "WHEN Requested GET /bankAccounts page without authentication"
+			+ "THEN returns expected redirect to (/login")
+    @Test
+    public void testGetBankAccountPageWithoutAuthentication() throws Exception {
+        mockMvc.perform(get("/bankAccounts"))
+                .andExpect(status().is(302))
+                .andExpect(redirectedUrlPattern("**/login"));
+
+    }
+    
+  //********************************************************************
+
+    
+    
+}
