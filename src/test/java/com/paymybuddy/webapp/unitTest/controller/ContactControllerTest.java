8914f13fd1856a26eff4732a37d0085c8a9598c7
selvaradjousk <senthil.selvaradjou@gmail.com>
2021-09-06 15:07:10 +0200

    [TEST] Controller - CONTACT : GET /contact request with AUTH 200 OK expected Http Response ==> FAILED

--- src/test/java/com/paymybuddy/webapp/unitTest/controller/ContactControllerTest.java
+++ src/test/java/com/paymybuddy/webapp/unitTest/controller/ContactControllerTest.java
@@ -1,8 +1,10 @@
 package com.paymybuddy.webapp.unitTest.controller;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -12,6 +14,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -41,7 +44,7 @@ class ContactControllerTest {
 	@DisplayName("GET /contact page Url request without authentication redirect /login - "
 			+ "GIVEN home url /contact "
 			+ "WHEN Requested GET /contact page"
-			+ "THEN returns expected reponse redirect to / login")  
+			+ "THEN returns expected reponse redirect to /login")  
 	@Test
 	public void testContactUrlWithoutLoginRedirectToLoginUrl() throws Exception {
 
@@ -51,7 +54,22 @@ class ContactControllerTest {
 	}
 
 	// ********************************************************************
+	@DisplayName("GET /contact page Url request with authentication 200 OK /contact - "
+			+ "GIVEN home url /contact "
+			+ "WHEN Requested GET /contact page"
+			+ "THEN returns expected reponse 200 OK to /contact") 
+	@WithMockUser("testemail1@email.com")
+	@Test
+	public void testContactUrlWithLoginStatusOK() throws Exception {
+
+		mockMvc.perform(get("/contact"))
+		 .andExpect(status().isOk())
+         .andExpect(view().name("contact"))
+         .andExpect(model().attribute("errorMessage", ""));
+		
+	}
 	
+	// ********************************************************************
 	
 	
 	
